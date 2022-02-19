c9190f43af4837d5d6193d1b5d3254f30e3d3beb
vermad843 <vishnuverma7524@gmail.com>
2022-02-20 03:28:25 +0530

    some css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
 
 function Header() {
   return (
-    <h2>I am a Header</h2>
+      <div className='header'>
+          <h2>I am a Header</h2>
+      </div>   
   )
 }
 
