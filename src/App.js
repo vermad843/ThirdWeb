3d3f0fee12c296ddc6eb804a0f61f2f6a989373f
vermad843 <vishnuverma7524@gmail.com>
2022-02-22 01:37:11 +0530

    CollectionCard component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import CollectionCard from './components/CollectionCard';
 
 function App() {
   return (
     <div className="app">
         <Header/>
+        <CollectionCard/>
     </div>
   );
 }
---
+++ src/components/CollectionCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CollectionCard.css'
+
+function CollectionCard() {
+  return (
+    <div className='collectionCard'>
+         CollectionCard
+    </div>
+  )
+}
+
+export default CollectionCard
