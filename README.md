8df5a9324a524bc13ea39cf946bb5b07f2f2e0d2
vermad843 <vishnuverma7524@gmail.com>
2022-02-23 03:11:57 +0530

    data in console

--- README.md
+++ README.md
@@ -13,3 +13,10 @@
 
 
 [x] [x] Maninder Image - https://i.ytimg.com/vi/4RTte4RA1Sg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCOdlLEgQtra4DmRSoMZCCCN8mKmw
+
+
+[x] Axios - npm i axios
+
+[x] API - 'https://testnets-api.opensea.io/assets?asset_contract_address=0x3C4a7FFe5655270F3AC7702768E4b17843d508F2&order_direction=asc'
+
+[x] BreakDown - 'https://testnets-api.opensea.io/assets?asset_contract_address=CPpunklink&order_direction=asc'
--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
 import Header from './components/Header';
 import CollectionCard from './components/CollectionCard';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [punkListData, setpunkListData] = useState([]);
+
+  useEffect (() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x3C4a7FFe5655270F3AC7702768E4b17843d508F2&order_direction=asc')
+      console.log(openseaData.data.assets);
+    }
+    return getMyNfts()
+  }, []);
   return (
     <div className="app">
         <Header/>
